c829d8a959b750920abe810955e687ad3f68ee96
Ethan Harsh <ethan@ethanharsh.com>
2024-07-20 16:06:40 -0400

    v0

--- generate.ts
+++ generate.ts
@@ -1,7 +1,8 @@
 import { readdir } from "fs/promises";
 import { extname } from "path";
 import ffmpeg from "fluent-ffmpeg";
-import Jimp from "jimp";
+// @ts-ignore
+import Vibrant = require("node-vibrant");
 
 const VIDEO_LENGTH = hoursToSeconds;
 
@@ -33,52 +34,12 @@ try {
   const videoLength = hoursToSeconds(videoLengthHours);
   const videoResolution = "3840x2160";
 
-  const command = ffmpeg();
-
-  command
-    .input(backgroundImagePath)
-    .loop(videoLength)
-    .input(audioFilePath)
-    .inputOptions(["-stream_loop -1"])
-    .complexFilter([
-      {
-        filter: "scale",
-        options: {
-          w: videoResolution.split("x")[0],
-          h: videoResolution.split("x")[1],
-          force_original_aspect_ratio: "increase",
-        },
-      },
-      {
-        filter: "pad",
-        options: {
-          w: "3840",
-          h: "2160",
-          x: "(ow-iw)/2",
-          y: "(oh-ih)/2",
-          color: await getScalingBarColor(backgroundImagePath),
-        },
-      },
-    ])
-    .videoCodec("libx264")
-    .audioCodec("aac")
-    .outputOptions(["-pix_fmt yuv420p", "-shortest"])
-    .output("ambient-video.mp4")
-    .on("start", (commandLine) => {
-      console.log("Spawned FFmpeg with command: " + commandLine);
-    })
-    .on("progress", (progress) => {
-      console.log(`Processing: ${progress.percent}% done`);
-    })
-    .on("end", () => {
-      console.log("Processing finished!");
-    })
-    .on("error", (err, stdout, stderr) => {
-      console.error("Error: " + err.message);
-      console.error("FFmpeg stdout: " + stdout);
-      console.error("FFmpeg stderr: " + stderr);
-    })
-    .run();
+  await generateVideo({
+    audioFilePath: `./audio/${audioFilePath}`,
+    backgroundImagePath: `./background/${backgroundImagePath}`,
+    videoLength,
+    videoResolution,
+  });
 
   process.exit(0);
 } catch (error) {
@@ -91,8 +52,72 @@ function hoursToSeconds(hours: number) {
 }
 
 async function getScalingBarColor(backgroundImagePath: string) {
-  const image = await Jimp.read(backgroundImagePath);
+  const vibrant = new Vibrant(backgroundImagePath);
+  const palette = await vibrant.getPalette();
+  const color = palette.DarkMuted?.getHex();
+  console.log("color", color);
+  return color;
+}
 
-  const dominantColor = image.getPixelColor(0, 0);
-  return Jimp.intToRGBA(dominantColor).toString();
+async function generateVideo({
+  audioFilePath,
+  backgroundImagePath,
+  videoLength,
+  videoResolution,
+}: {
+  audioFilePath: string;
+  backgroundImagePath: string;
+  videoLength: number;
+  videoResolution: string;
+}) {
+  return new Promise<void>(async (resolve, reject) => {
+    const command = ffmpeg();
+
+    command
+      .input(backgroundImagePath)
+      .loop(videoLength)
+      .input(audioFilePath)
+      .inputOptions(["-stream_loop -1"])
+      .complexFilter([
+        {
+          filter: "scale",
+          options: {
+            w: videoResolution.split("x")[0],
+            h: videoResolution.split("x")[1],
+            force_original_aspect_ratio: "increase",
+          },
+        },
+        // {
+        //   filter: "pad",
+        //   options: {
+        //     w: "3840",
+        //     h: "2160",
+        //     x: "(ow-iw)/2",
+        //     y: "(oh-ih)/2",
+        //     color: await getScalingBarColor(backgroundImagePath),
+        //   },
+        // },
+      ])
+      .videoCodec("libx264")
+      .audioCodec("aac")
+      .outputOptions(["-pix_fmt yuv420p", "-shortest"])
+      .output("./ambient-video.mp4")
+      .on("start", (commandLine) => {
+        console.log("Spawned FFmpeg with command: " + commandLine);
+      })
+      .on("progress", (progress) => {
+        console.log(`Processing: ${progress.percent}% done`);
+      })
+      .on("end", () => {
+        console.log("Processing finished!");
+        resolve();
+      })
+      .on("error", (err, stdout, stderr) => {
+        console.error("Error: " + err.message);
+        console.error("FFmpeg stdout: " + stdout);
+        console.error("FFmpeg stderr: " + stderr);
+        reject();
+      })
+      .run();
+  });
 }
